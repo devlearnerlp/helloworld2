7800269a7aa220f3e024bfc1db15547204152d09
kpraghav <kpraghav@gmail.com>
2023-08-19 23:05:41 +0530

    removed sysout

--- src/main/java/com/example/demo/Helloworld2Application.java
+++ src/main/java/com/example/demo/Helloworld2Application.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Helloworld2Application {
 
 	public static void main(String[] args) {
-		System.out.println("into the main class");
-		System.out.println("into the main class");
+		System.out.println("into the main class");		
 		SpringApplication.run(Helloworld2Application.class, args);
 		
 	}
